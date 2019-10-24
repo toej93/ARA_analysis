6ccd917664941fee4b4ea1677efaa3cfa989a87b
Jorge Torres <torresespinosa.1@osu.edu>
2019-10-24 13:12:46 -0400

    Added reco for double-pulse events

--- Reco/reco_debug.cxx
+++ Reco/reco_debug.cxx
@@ -167,7 +167,7 @@ int main(int argc, char **argv)
 	for (int i = 0; i < numRadiiScanned; i++){
 		double radius_temp = radii[i+startingRadiusBin];
 		cout << "Setup RTCorr : " << radius_temp << endl;
-		theCorrelators[i] = new RayTraceCorrelator(station_num, radius_temp, settings, 0.25, RTTestMode);
+		theCorrelators[i] = new RayTraceCorrelator(station_num, radius_temp, settings, 1.0, RTTestMode);
 	}
 	double weight;
 	int unixTime;
