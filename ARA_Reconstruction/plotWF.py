4108996da1e013edfec09b770d7d9fc30a552003
Jorge Torres <torresespinosa.1@osu.edu>
2021-02-02 10:28:03 -0500

    Removing unnecessary files

--- ARA_Reconstruction/plotWF.py
+++ ARA_Reconstruction/plotWF.py
@@ -16,63 +16,48 @@ from ROOT import gInterpreter, gSystem
 from ROOT import TChain, TSelector, TTree
 import scipy
 import sys
-import warnings
-warnings.filterwarnings("ignore")
+# import warnings
+# warnings.filterwarnings("ignore")
 
 
-file_list=[]#Define an empty list
-for filename in os.listdir("/fs/scratch/PAS0654/jorge/sim_results/noiseOn"):#Loop over desired directory
-        if filename.startswith("AraOut.default_A2_c1_E600.txt.run40"): #extension, .root in this case
-            file_list.append(os.path.join("/fs/scratch/PAS0654/jorge/sim_results/noiseOn", str(filename))) #add file name to the list
+
+gSystem.Load('libAraEvent.so') #load the simulation event library. You might get an error asking for the eventSim dictionry. To solve that, go to where you compiled AraSim, find that file, and copy it to where you set LD_LIBRARY_PATH.
 
 
-noise=True
-eventTree = TChain("eventTree") #Define chain and tree that needs to be read. "VTree" in this case.
-SimTree = TChain("AraTree2")
+test = ROOT.TFile.Open("/fs/scratch/PAS0654/brian/L1/ARA02/1223/run_012549/event012549.root")#directory where the simulation files are
 
-for line in file_list:
-    eventTree.AddFile(line)
-    SimTree.AddFile(line)
 
-reportPtr = ROOT.Report()#report pointer
-eventPtr = ROOT.Event()
-
-# eventTree = test.Get("eventTree")#eventTree, from AraSim output files
-# SimTree = test.Get("AraTree2") #AraTree2, from AraSim output files
-rawEvent = ROOT.UsefulAtriStationEvent()
-eventTree.SetBranchAddress("UsefulAtriStationEvent",ROOT.AddressOf(rawEvent))
-SimTree.SetBranchAddress("report",ROOT.AddressOf(reportPtr))
-SimTree.SetBranchAddress("event", ROOT.AddressOf(eventPtr))
-
+calibrator = ROOT.AraEventCalibrator.Instance()
+eventTree = test.Get("eventTree")
+rawEvent = ROOT.RawAtriStationEvent()
+eventTree.SetBranchAddress("event",ROOT.AddressOf(rawEvent))
 totalEvents = eventTree.GetEntries()
 print('total events:', totalEvents)
 isTrue=False
-evNum = int(sys.argv[1])
-for i in range(evNum,totalEvents):#loop over events
-    # if(isTrue):
-    #     break
-    eventTree.GetEntry(i)
-    SimTree.GetEntry(i)
-    if(reportPtr.stations[0].Global_Pass <= 0):#making sure that the event did trigger, otherwise there won't be a waveform (this might not be needed if all waveforms are saved)
-        continue
-    # Check if any of the top antennas has no signal. Reject those events
-    
+
+for evNum in range(0,9):#loop over events
+#     if( isTrue):
+#         break
+    eventTree.GetEntry(evNum)
+#     if(rawEvent.isCalpulserEvent()==0): #if not a cal pulser
+#         continue
+    usefulEvent = ROOT.UsefulAtriStationEvent(rawEvent,ROOT.AraCalType.kLatestCalib)#get useful event
+#     gr1 = usefulEvent.getGraphFromElecChan(0)#print waveform
+# 
     gr = [None]*16
-    fig, axs = plt.subplots(4, 4, figsize = (12,10))
+    fig, axs = plt.subplots(4, 4, figsize = (11,9))
     axs = axs.ravel()
     for ch in range(0,16):
         t = []
         v = []
-        gr[ch] = rawEvent.getGraphFromRFChan(ch)#print waveform
+        gr[ch] = usefulEvent.getGraphFromRFChan(ch)#print waveform
         for kk in range(0,gr[ch].GetN()):
           t.append(gr[ch].GetX()[kk])
           v.append(gr[ch].GetY()[kk])
         axs[ch].plot(t,v,linewidth=0.5, label = "Ch %i"%ch)
         axs[ch].legend()
-        if(ch==0):
-            print(np.array(v).std())
         plt.grid(which="both")
         axs[ch].set_ylim(-600,600)
     plt.tight_layout()
-    plt.savefig("/users/PAS0654/osu8354/ARA_cvmfs/source/AraRoot/analysis/thesis_work_daily/plots/Dumpster/wf_all_ev%i.png"%evNum, dpi=100)
-    break
+    plt.savefig("./wf_all_ev%i.png"%evNum, dpi=200)
+    # break
