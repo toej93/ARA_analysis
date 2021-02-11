84b53d5ae0975a6cb7f7e56f6ae4fff987dbc412
Jorge Torres <torresespinosa.1@osu.edu>
2021-02-11 15:09:44 -0500

    Adding version that stores power

--- CenA_sourceSearch/Stokes/createHDF5ForSeckel.py
+++ CenA_sourceSearch/Stokes/createHDF5ForSeckel.py
@@ -1,9 +1,9 @@
 """
-#####deDisperse.py#####
+#####createHDF5ForSeckel.py#####
 
-Get de-dispersed waveform from AraSim events with Python
+Create dataset witj AraSim outputs for Seckel
 Author: Jorge Torres
-Date: Jul 28, 2020.
+Date: Feb 11, 2021.
 """
 from ROOT import TCanvas, TGraph
 from ROOT import gROOT
@@ -31,20 +31,15 @@ gInterpreter.ProcessLine('#include "/users/PAS0654/osu8354/AraSim/Settings.h"')
 
 gSystem.Load('/users/PAS0654/osu8354/AraSim/libAra.so') #load the simulation event library. You might get an error asking for the eventSim dictionry. To solve that, go to where you compiled AraSim, find that file, and copy it to where you set LD_LIBRARY_PATH.
 
-
-# test = ROOT.TFile.Open("/fs/scratch/PAS0654/jorge/sim_results/CenA_atzero/AraOut.CenA_fixed_source_seed4.txt.run0.root")#directory where the simulation files are
-# test = ROOT.TFile.Open("/fs/scratch/PAS0654/jorge/sim_results/default/AraOut.default_A2_c1_E610.txt.run9.root")
-
 file_list=[]#Define an empty list
-for filename in os.listdir("/fs/scratch/PAS0654/jorge/sim_results/noiseOn"):#Loop over desired directory
-        if filename.startswith("AraOut.default_A2_c1_E%s.txt.run%s.root"%(sys.argv[1],sys.argv[2])): #extension, .root in this case
-            file_list.append(os.path.join("/fs/scratch/PAS0654/jorge/sim_results/noiseOn", str(filename))) #add file name to the list
-
-
+# for filename in os.listdir("/fs/scratch/PAS0654/jorge/sim_results/noiseOn"):#Loop over desired directory
+#         if filename.startswith("AraOut.default_A2_c1_E%s.txt.run%s"%(sys.argv[1],sys.argv[2])): #extension, .root in this case
+#             file_list.append(os.path.join("/fs/scratch/PAS0654/jorge/sim_results/noiseOn", str(filename))) #add file name to the list
+# 
+file_list.append("/users/PAS0654/osu8354/AraSim/outputs/AraOut.default_A2_c1_E610_readIn_noise.txt.runAraSim_input_Seckel.txt.root")
 noise=True
 eventTree = TChain("eventTree") #Define chain and tree that needs to be read. "VTree" in this case.
 SimTree = TChain("AraTree2")
-energy_=(int(sys.argv[1])-400)/10
 
 for line in file_list:
     eventTree.AddFile(line)
@@ -84,8 +79,8 @@ batch = []
 weight_arr = []
 view_ang = []
 R_recoSign = []
-PeakE_arr = []
-PeakV_arr = []
+powerE_arr = []
+powerV_arr = []
 dirProp = []
 nnu = []
 launch_ang = []
@@ -97,39 +92,64 @@ hadfrac = []
 flavor = []
 nu_nubar = []
 energy = []
-
+powerEField_arr = []
+powerNoise_arr = []
 
 for i in range(0,totalEvents):#loop over events
-
+    print(i)
     eventTree.GetEntry(i)
     SimTree.GetEntry(i)
     if(reportPtr.stations[0].Global_Pass <= 0):#making sure that the event did trigger, otherwise there won't be a waveform (this might not be needed if all waveforms are saved)
         continue
 
-    try:
-        # whichSol = reportPtr.stations[0].strings[0].antennas[0].Likely_Sol #0: direct, #1: reflected/refracted
-        whichSol = 0
-        # if(whichSol!=0):#If it can't pick what solution triggered, AraSim returns -1
-            # continue
-        theta_antenna_ = reportPtr.stations[0].strings[0].antennas[0].theta_rec[whichSol]
-        phi_ant = reportPtr.stations[0].strings[0].antennas[0].phi_rec[whichSol]
+    whichSol = 0
 
+    launchAngAnt = np.empty(16)
+    phiRecAnt = np.empty(16)
+    thetaRecAnt = np.empty(16)
+    viewAngAnt = np.empty(16)
+    pol_x = np.empty(16)
+    pol_y = np.empty(16)
+    pol_z = np.empty(16)
+    powerEField = np.empty(16)
+    try:
+        
+        for string in range(0,4):
+            for chan in range(0,4):
+                RFchan = util.getRFChannel(string, chan)
+                
+                pol_x[RFchan] = reportPtr.stations[0].strings[string].antennas[chan].Pol_vector[whichSol].GetX()
+                pol_y[RFchan] = reportPtr.stations[0].strings[string].antennas[chan].Pol_vector[whichSol].GetY()
+                pol_z[RFchan] = reportPtr.stations[0].strings[string].antennas[chan].Pol_vector[whichSol].GetZ()
+                            
+                launchAngAnt[RFchan] = reportPtr.stations[0].strings[string].antennas[chan].launch_ang[whichSol]
+                
+                thetaRecAnt[RFchan] = reportPtr.stations[0].strings[string].antennas[chan].theta_rec[whichSol]
+                phiRecAnt[RFchan] = reportPtr.stations[0].strings[string].antennas[chan].phi_rec[whichSol]
+                
+                viewAngAnt[RFchan] = reportPtr.stations[0].strings[string].antennas[chan].view_ang[whichSol]
+                
+                wfLength = int(reportPtr.stations[0].strings[string].antennas[chan].Vm_zoom[whichSol].size())
+                
+                Efield_v = []
+                Efield_t = []
+
+                for sample in range(wfLength):
+                    Efield_t.append(reportPtr.stations[0].strings[string].antennas[chan].Vm_zoom_T[whichSol][sample])
+                    Efield_v.append(reportPtr.stations[0].strings[string].antennas[chan].Vm_zoom[whichSol][sample])
+                
+                Efield_t = np.array(Efield_t)
+                Efield_v = np.array(Efield_v)
+                dT = Efield_t[1]-Efield_t[0]
+                power = np.sum(Efield_v**2)*dT
+                powerEField[RFchan] = power
     except:
         continue
-
-    try:
-        polVec_x_ = reportPtr.stations[0].strings[0].antennas[0].Pol_vector[whichSol].GetX()
-        polVec_y_ = reportPtr.stations[0].strings[0].antennas[0].Pol_vector[whichSol].GetY()
-        polVec_z_ = reportPtr.stations[0].strings[0].antennas[0].Pol_vector[whichSol].GetZ()
-    except:
-        continue
-
-    gr = [None]*8
-    
     plotting = False
 
-    peakE = []
-    peakV = []
+    powerE = []
+    powerV = []
+    powerNoise = []
     for ch in range(0,16):
         t = []
         v = []
@@ -139,67 +159,48 @@ for i in range(0,totalEvents):#loop over events
           v.append(gr.GetY()[k])
     # plt.title("An example of a triggered simulated event with Python")
         if(ch<8):
-            deConv_t,deConv_v = util.deConvolve_antennaAraSim(t, v,theta_antenna_, phi_ant, 0)
-
-            # deConv_t,deConv_v = util.deConvolve_antennaAraSim(t, v,np.deg2rad(theta_antenna_), np.deg2rad(vertex[2]), 0)
-            # maxV = max(abs(deConv_v))
-            # if(noise == False):
-            #     maxV = util.findMaxSign(np.array(deConv_v))
-            #     rmsV_ = max(abs(np.array(v)))
-            # else:
-            #     maxV = util.findMaxSign(np.array(v))
-            #     rmsV_ = np.array(v[0:60]).std()
-            peakV.append(util.findMaxSign(np.array(v)))
-            peakE.append(util.findMaxSign(np.array(deConv_v)))
-            # dTV = deConv_t[1]-deConv_t[0]
-            # powerV = np.sum(deConv_v**2)*dTV
-            # powerV = util.integratePowerWindow(deConv_t,deConv_v)-util.integratePowerNoise(deConv_t,deConv_v)
+            deConv_t,deConv_v = util.deConvolve_antennaAraSim(t, v, thetaRecAnt[ch], phiRecAnt[ch], 0)
+            
+            powerV.append(util.integratePowerWindow(t,v))
+            powerE.append(util.integratePowerWindow(deConv_t,deConv_v))
+            if(noise == True):
+                powerNoise.append(util.integratePowerNoise(deConv_t,deConv_v))
+            else:
+                powerNoise.append(0.)            
             
         else:
-            deConv_t,deConv_v = util.deConvolve_antennaAraSim(t, v,theta_antenna_, phi_ant, 1)
+            deConv_t,deConv_v = util.deConvolve_antennaAraSim(t, v, thetaRecAnt[ch], phiRecAnt[ch], 1)
 
-            # if(noise == False):
-            #     maxH = util.findMaxSign(np.array(deConv_v))
-            #     rmsH_ = max(abs(np.array(v)))
-            # else:
-            #     maxH = util.findMaxSign(np.array(v))
-            #     rmsH_ = np.array(v[0:60]).std()
-            peakV.append(util.findMaxSign(np.array(v)))
-            peakE.append(util.findMaxSign(np.array(deConv_v)))
-            # dTH = deConv_t[1]-deConv_t[0]
-            # powerH = np.sum(deConv_v**2)*dTH
-            # powerH = util.integratePowerWindow(deConv_t,deConv_v)-util.integratePowerNoise(deConv_t,deConv_v)
-            # PeakH = util.findMaxSign(np.array(deConv_v))
-
-    # dirProp.append(np.array([np.sin(theta_antenna_)*np.cos(phi_ant),np.sin(theta_antenna_)*np.sin(phi_ant),np.cos(theta_antenna_)]))
-    
+            powerV.append(util.integratePowerWindow(t,v))
+            powerE.append(util.integratePowerWindow(deConv_t,deConv_v))
+            if(noise == True):
+                powerNoise.append(util.integratePowerNoise(deConv_t,deConv_v))
+            else:
+                powerNoise.append(0.)
     evt_num.append(i)
-    polVec_x.append(polVec_x_)
-    polVec_y.append(polVec_y_)
-    polVec_z.append(polVec_z_)
-    # rmsV.append(rmsV_)
-    # rmsH.append(rmsH_)
-    # maxV_array.append(maxV)
-    # maxH_array.append(maxH)
-    # powerVArr.append(powerV)
-    # powerHArr.append(powerH)
-    theta_antenna.append(theta_antenna_)
-    phi_antenna.append(phi_ant)
-    # PolVecReco = util.PolVectorReco(PeakV,PeakH,theta_antenna_, phi_ant)
-    # PolVecReco_array.append(PolVecReco)
-    PolVecTrue_array.append(np.array([polVec_x_,polVec_y_,polVec_z_]))
-    energyArr.append(energy_)
+    polVec_x.append(pol_x)
+    polVec_y.append(pol_y)
+    polVec_z.append(pol_z)
+
+    theta_antenna.append(thetaRecAnt)
+    phi_antenna.append(phiRecAnt)
+
+    energyArr.append(eventPtr.pnu)
     weight = eventPtr.Nu_Interaction[0].weight
     weight_arr.append(weight)
-    view_ang.append(reportPtr.stations[0].strings[0].antennas[0].view_ang[whichSol])
-    # R_recoSign.append(np.sign(PeakH/PeakV))
-    PeakV_arr.append(peakV)
-    PeakE_arr.append(peakE)
+    view_ang.append(viewAngAnt)
+    powerV_arr.append(powerV)
+    powerE_arr.append(powerE)
+    powerNoise_arr.append(powerNoise)
 
-    # Peak_H_arr.append(PeakH)
-    launch_ang.append(reportPtr.stations[0].strings[0].antennas[0].launch_ang[whichSol])
+    # polVec_x.append(pol_x)
+    # polVec_y.append(pol_y)
+    # polVec_z.append(pol_z)
+    powerEField_arr.append(powerEField)
+
+    launch_ang.append(launchAngAnt)
     nnu.append(np.array([eventPtr.Nu_Interaction[0].nnu.GetX(),eventPtr.Nu_Interaction[0].nnu.GetY(),eventPtr.Nu_Interaction[0].nnu.GetZ()]))   
-    # posnu.append(np.array([eventPtr.Nu_Interaction[0].posnu.GetX(),eventPtr.Nu_Interaction[0].posnu.GetY(),eventPtr.Nu_Interaction[0].posnu.GetZ()]))
+    posnu.append(np.array([eventPtr.Nu_Interaction[0].posnu.GetX(),eventPtr.Nu_Interaction[0].posnu.GetY(),eventPtr.Nu_Interaction[0].posnu.GetZ()]))
     current.append(eventPtr.Nu_Interaction[0].currentint)
     inelasticity.append(eventPtr.Nu_Interaction[0].elast_y)
     emfrac.append(eventPtr.Nu_Interaction[0].emfrac)
@@ -208,7 +209,7 @@ for i in range(0,totalEvents):#loop over events
     nu_nubar.append(eventPtr.nu_nubar)
     energy.append(eventPtr.pnu)
     
-with h5py.File('AraSim_outputs_forSeckel.hdf5', 'w') as f:
+with h5py.File('./hdf_files/AraSim_outputs_forSeckel_1E18_noise_v2.hdf5', 'w') as f:
     #Event properties
     f.create_dataset('evNum', data=np.array(evt_num))
     f.create_dataset('current', data=np.array(current))
@@ -220,15 +221,20 @@ with h5py.File('AraSim_outputs_forSeckel.hdf5', 'w') as f:
     f.create_dataset('nu_nubar', data=np.array(nu_nubar))
     f.create_dataset('posnu', data=np.array(posnu))
     f.create_dataset('weight', data=np.array(weight_arr))
-    
+    f.create_dataset('energy', data=np.array(energy))
+
     #Signal properties per antenna
     f.create_dataset('launch_ang', data=np.array(launch_ang))
     f.create_dataset('theta_antenna', data=np.array(theta_antenna))
     f.create_dataset('phi_antenna', data=np.array(phi_antenna))
     f.create_dataset('view_ang', data=np.array(view_ang))
+    f.create_dataset('pol_x', data=np.array(polVec_x))
+    f.create_dataset('pol_y', data=np.array(polVec_y))
+    f.create_dataset('pol_z', data=np.array(polVec_z))
+    f.create_dataset('powerV_deconv', data=np.array(powerE_arr))
+    f.create_dataset('powerV', data=np.array(powerV_arr))
+    f.create_dataset('powerE', data=np.array(powerEField_arr))
+    f.create_dataset('powerNoise_arr', data=np.array(powerNoise_arr))
 
 
     f.close()
-    # print(vertex[1])
-# original_df = pd.DataFrame({"EvNum":np.array(evt_num),"theta_reco": np.array(theta_antenna), "phi_reco": np.array(phi_antenna), "PolTrue":PolVecTrue_array,"PolReco":PolVecReco_array,"rmsV":np.array(rmsV),"rmsH":np.array(rmsH),"maxV":np.array(maxV_array),"maxH":np.array(maxH_array),"powerV":np.array(powerVArr),"powerH":np.array(powerHArr),"energyArr":np.array(energyArr),"batch":np.array(batch),"weight":np.array(weight_arr),"view_ang":np.array(view_ang),"R_recoSign":np.array(R_recoSign),"peak_V":np.array(Peak_V),"peak_H":np.array(Peak_H),"dirProp":dirProp,"nnu":nnu,"launch_ang":launch_ang})
-# original_df.to_pickle("./noiseOn/window_Sol0/pol_quant_noise_1E%0.1f_%s.pkl"%(energy_,sys.argv[2]))
