7d27dd86e0e6e56031e85d0f5f4c833594ef3840
Jorge Torres <torresespinosa.1@osu.edu>
2021-06-25 13:48:46 -0400

    Adding script that submits jobs via a loop

--- Reco_sim/reco_code/submit_reco_sim_loop.sh
+++ Reco_sim/reco_code/submit_reco_sim_loop.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
 #SBATCH --job-name=run_reco_sim_radiiLoop
-#SBATCH --nodes=1 --ntasks-per-node=15
+#SBATCH --nodes=1 --ntasks-per-node=1
 #SBATCH --account=PAS0654
-#SBATCH --mail-type=END,FAIL
-#SBATCH --time=06:35:00
-#SBATCH --output=run_reco_sim_rad19.log   # Standard output and error log
+#SBATCH --mail-type=FAIL
+#SBATCH --time=01:35:00
+#SBATCH --array=101-399%100
+#SBATCH --output=logs/run_reco_sim_radiiLoop_%A-%a.out    # Standard output and error log
+
 eval 'source /users/PCON0003/cond0068/.bash_profile_pitzer_cvmfs'
 
 cd /users/PAS0654/osu8354/ARA_cvmfs/source/AraRoot/analysis/
-# RADIUS=6
-RADIUS=19
 
-FILES=/fs/scratch/PAS0654/jorge/sim_results/simplified/*.root
-for f in $FILES
-  do
-    (
-      ./reco_interf_loopRadii 1 2 1 0 /users/PAS0654/osu8354/ARA_cvmfs/source/AraRoot/analysis/ARA_analysis/Reco_sim/reco_code/Interf_files/filter_files /users/PAS0654/osu8354/ARA_cvmfs/source/AraRoot/analysis/ARA_analysis/Reco_sim/reco_code/Interf_files/loop "$f" 
-    )&
-    if (( $(wc -w <<<$(jobs -p)) % 15 == 0 )); then wait; fi #Only submit 15 jobs at a time
-  done
-wait
+./reco_interf_loopRadii 1 2 1 0 /users/PAS0654/osu8354/ARA_cvmfs/source/AraRoot/analysis/ARA_analysis/Reco_sim/reco_code/Interf_files/filter_files /users/PAS0654/osu8354/ARA_cvmfs/source/AraRoot/analysis/ARA_analysis/Reco_sim/reco_code/Interf_files/loop/ /fs/scratch/PAS0654/jorge/sim_results/simplified/AraOut.default_A2_c1_E580.txt.run"$SLURM_ARRAY_TASK_ID".root 
